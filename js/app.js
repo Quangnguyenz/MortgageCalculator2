19f9f5609e21af838266c7b1ca31b1d5a6ccad06
Quangnguyenz <81808085+Quangnguyenz@users.noreply.github.com>
2021-09-22 16:00:02 -0500

    Initial commit

---
+++ js/app.js
@@ -0,0 +1,6 @@
+let state = {
+    price: document.querySelectorAll('[name="price"]')[0].value,
+    loan_years: document.querySelectorAll('[name="loan_years"]')[0].value
+    dow
+}
+console.log(state)
